8d63cd5492112c2039241e78cb4ac4c5d2201777
cjoraeki <cjoraeki3@gmail.com>
2022-12-10 02:18:17 +0100

    added docker configurations

--- src/main/java/com/example/volunteer/entity/User.java
+++ src/main/java/com/example/volunteer/entity/User.java
@@ -3,7 +3,6 @@ package com.example.volunteer.entity;
 
 import com.example.volunteer.enums.Status;
 import lombok.*;
-
 import javax.persistence.*;
 
 
--- src/test/java/com/example/volunteer/services/UserServiceTest.java
+++ src/test/java/com/example/volunteer/services/UserServiceTest.java
@@ -1,4 +1,12 @@
+package com.example.volunteer.services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserServiceTest {
-  
+
+    @Test
+    void createUser() {
+    }
 }
